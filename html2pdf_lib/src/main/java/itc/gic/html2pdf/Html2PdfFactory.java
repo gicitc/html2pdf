86cdd56d7e7648cf5922a7ad5597a2fbb14ba278
srengtal <srengty@gmail.com>
2017-08-19 15:56:11 +0700

    setup project

--- html2pdf_lib/src/main/java/itc/gic/html2pdf/Html2PdfFactory.java
+++ html2pdf_lib/src/main/java/itc/gic/html2pdf/Html2PdfFactory.java
@@ -1,8 +1,14 @@
 package itc.gic.html2pdf;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+
 /**
  * Created by Institute on 8/19/2017.
  */
 
 public class Html2PdfFactory {
+    public static void htmlText2pdf(String text){
+        //PdfWriter.getInstance( )
+    }
 }
