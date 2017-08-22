ed4e555930a12095106ebf45e60092b66ed55bd4
srengtal <srengty@gmail.com>
2017-08-22 12:37:27 +0700

    add method string to pdf

--- app/src/main/java/itc/gic/html2pdf/app/MainActivity.java
+++ app/src/main/java/itc/gic/html2pdf/app/MainActivity.java
@@ -1,13 +1,67 @@
 package itc.gic.html2pdf.app;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.itextpdf.text.DocumentException;
+
+import java.io.File;
+import java.io.IOException;
+
+import itc.gic.html2pdf.Html2PdfFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private EditText etSearch;
+    private String content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.wv_main);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:window.HTMLOUT.processHTML(document.getElementsByTagName('html')[0].innerHTML)");
+            }
+        });
+
+        etSearch = (EditText) findViewById(R.id.et_search);
+    }
+
+    public void doSearch(View view) {
+        webView.loadUrl(etSearch.getText().toString());
+    }
+
+    public void doToPdfNoScript(View view) {
+        File dir = Environment.getExternalStoragePublicDirectory("Download");
+        try {
+            Html2PdfFactory.htmlText2pdf(content, new File(dir, "html.pdf").getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this, content, Toast.LENGTH_LONG).show();
+    }
+    class MyJavaScriptInterface {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processHTML(String html){
+            content = html;
+        }
     }
 }
--- html2pdf_lib/src/main/java/itc/gic/html2pdf/Html2PdfFactory.java
+++ html2pdf_lib/src/main/java/itc/gic/html2pdf/Html2PdfFactory.java
@@ -1,14 +1,34 @@
 package itc.gic.html2pdf;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 
 /**
  * Created by Institute on 8/19/2017.
  */
 
 public class Html2PdfFactory {
-    public static void htmlText2pdf(String text){
-        //PdfWriter.getInstance( )
+    public static void htmlText2pdf(String text, String filePDF) throws IOException, DocumentException {
+        Document document = new Document();
+        FileOutputStream os = new FileOutputStream(filePDF);
+        PdfWriter pdfWriter = PdfWriter.getInstance(document, os);
+        document.open();
+
+        /*HTMLWorker htmlWorker = new HTMLWorker(document);
+        htmlWorker.parse(new StringReader(text));*/
+        InputStream is = new ByteArrayInputStream(text.getBytes());
+        XMLWorkerHelper.getInstance().parseXHtml(pdfWriter, document, is);
+
+        document.close();
+        os.close();
     }
 }
